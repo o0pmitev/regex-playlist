eb5b1e029d4e316b7b7068e64ebf00d62a1dd607
o0pmitev <pmitev89@gmail.com>
2020-07-20 10:05:08 +0200

    add number validation

--- validation.js
+++ validation.js
@@ -1,1 +1,28 @@
 // validation script here
+// let reg = /[a-z]{5-12}/gi;
+// let reg2 = new RegExp(/[a-z]{5-12}/, 'i')
+
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+  telephone: /^\d{11}$/
+};
+
+// validation function
+
+function validate(field, regex) {
+  if(regex.test(field.value)) {
+    field.className = 'valid';
+  } else {
+    field.className = 'invalid'
+  }
+}
+
+
+inputs.forEach((input) => {
+  input.addEventListener('keyup', e => {
+    // console.log(e.target.attributes.name.value)
+    validate(e.target, patterns[e.target.attributes.name.value])
+  })
+})
+
